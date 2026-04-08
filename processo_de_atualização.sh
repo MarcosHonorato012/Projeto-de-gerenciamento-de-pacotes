7816190956692f1ec933fdbe032db65237dc81e8
Rafael <marcos.honorato012@example.com>
2026-04-07 23:14:37 -0300

    atualização do processo de atulizar + documentação

--- processo_de_atualização.sh
+++ processo_de_atualização.sh
@@ -1,66 +1,88 @@
 #! /bin/bash
+######################################################################################################################################
+#                                                                                                                                    #
+#                                                                                                                                    #
+#                 NESSE CODIGO VAI CONTER AUTOMAÇÃO DE ATUALIZAR O SISTEMA E UM PACOTE ESPECIFICO.                                    #
+#                                                                                                                                    #
+#                                                                                                                                    #
+#                                                                                                                                    #
+#                                                                                                                                    #
+######################################################################################################################################
 atualizar_os_pacotes(){
 	while_do_instalador=1 #Para iniciar o while#
 
  	while [ "$while_do_instalador" = "1" ]; do   #Perguntas para iniciar o processo# 
 
-		if sudo apt-get update -y | grep "Todos os pacotes estão atualizados."
+		if sudo apt-get update -y | grep "Todos os pacotes estão atualizados."# verifica se o sistema está atualizado#
 		then
-			echo "Seu sistema já está totalmente atualizado. Processo de atualização foi terminado"
+			echo "Seu sistema já está totalmente atualizado. Processo de atualização foi terminado" #finaliza o processo#
 
 			break
 		else
-			echo "Deseja atualizar um pacote especifico? s/n"
+			echo "Deseja atualizar um pacote especifico? s/n" #Pergunta se usuario quer um pacote especifico para atualizar
 
 			read atualizar_pacote_especifico
 
                         echo
 
-			if [ "$atualizar_pacote_especifico" = "s" ]
+			if [ "$atualizar_pacote_especifico" = "s" ] #verifica se o usuario quer atualizar#
 			then
-				echo "Digite o nome do pacote"
+				echo "Digite o nome do pacote" #Recebe o pacote especifico#
 
 				read pacote_especifico
 
 				echo
 
-				if apt-cache show "$pacote_especifico" > /dev/null 2>&1
+				if apt-cache show "$pacote_especifico" > /dev/null 2>&1  #verifica se escreveu certo#
 
 				then
 
-					sudo apt-get upgrade -y "$pacote_especifico"
+					sudo apt-get upgrade -y "$pacote_especifico" #atualiza pacote especifoco
+
+					echo
 
 					echo "Atualização feita com sucesso"
 
 					echo
 
-					echo "Deseja atualizar o sistema? s/n"
+					echo "Deseja atualizar o sistema? s/n" #Recebe se usuario que atualizar o sistema como todo#
 
 					echo
 
 					read sistema_atualizar
 						
-					if [ "$sistema_atualizar" = "s"]
+					if [ "$sistema_atualizar" = "s"] #Atualiza o sistema ou termina o processo esse processo#
 					then
-						sudo apt update -y && sudo apt-get upgrade -y
+						sudo apt update -y && sudo apt-get upgrade -y #Atualiza o sistema
+
+						echo 
+
+                                                #Termina esse processo#
 
 						echo "Atualização no sistema completa. Processo de atualização concluida."
-						break
+
+						break 
 
 					else
 						
 						echo "Processo de atualização concluida. Processo de atualização concluida"
+
+                                                  #Termina o processo#
+
 						break
 					fi
 				else 
-					echo "Erro... Provavelmente errou a digitaçõa. Digite novamente"
+	                                   
+					#Avisa o erro para usuario, pois, ele erro nome do pacote#
+
+					echo "Erro... Provavelmente errou a digitaçõa. Digite novamente" 
 				fi
 			else
-				sudo apt update -y && sudo apt upgrade -y
+				sudo apt update -y && sudo apt upgrade -y #Atualiza o sistem#
 				
 				echo
 
-				echo "Sistema atualizado. Processo de atualização concluida"
+				echo "Sistema atualizado. Processo de atualização concluida" #Termina esse processo#
 
 				break
 			fi	
